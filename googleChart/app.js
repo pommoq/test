d52b26cb41129a9b4d5e630c4cbb31e8353ead3b
pommoq <pom.moq@hotmail.com>
2015-10-13 09:19:18 +0700

    เพิ่มตัวอย่าง chart

--- googleChart/app.js
+++ googleChart/app.js
@@ -2,13 +2,11 @@ google.load('visualization', '1', {'packages': ['geochart']});
 google.setOnLoadCallback(drawRegionsMap);
 
 function drawRegionsMap() {
+    // data ตัวหน้าเป็น รหัส หรือ ชื่อก็ได้ ดูจาก https://en.wikipedia.org/wiki/ISO_3166-2:TH    
 	var data = google.visualization.arrayToDataTable([
-        ['province', 'valueTitle'],
-        /* ตัวหน้าเป็น รหัส หรือ ชื่อก็ได้ ดูจาก 
-           https://en.wikipedia.org/wiki/ISO_3166-2:TH*/
+        ['province', 'valueTitle'],  // ตัวแรกเหมือนจะเป็น Caption
         ['TH-57' , 883], 
-        /* ชื่อ เป็น ไทย หรือ Eng ก็ได้ (แต่ต้องสะกดถูกต้อง และเป็นฃื่อทางการ */
-        ['Chiang Mai' , 1784],
+        ['Chiang Mai' , 1784], // ชื่อ เป็น ไทย หรือ Eng ก็ได้ (แต่ต้องสะกดถูกต้อง และเป็นฃื่อทางการ 
         ['กรุงเทพมหานคร' , 784],
         ['ลำปาง' , 619],
         ['ลำพูน' , 619],
@@ -18,7 +16,7 @@ function drawRegionsMap() {
     ]);
 	var options = {'region':'TH',
                    'resolution':'provinces' ,
-                   'colors': ['#FF0000', '#00FF00']
+                   //'colors': ['#FF0000', '#00FF00']
 	};
     var chart = new google.visualization.GeoChart(
                             document.getElementById('chart_div')
